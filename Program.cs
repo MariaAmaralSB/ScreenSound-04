e58f3b1d16c36887c8ab3f5f1d2bba990d6716a4
Maria Eduarda Amaral <mariaeduardaamaralsb@gmail.com>
2024-04-29 18:05:29 -0300

    Alterado programa principal

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
 
         Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musicas>>(resposta)!;
+        musicas [1998].ExibirDetalhesdaMusica();
     }
     catch (Exception ex)
     {
