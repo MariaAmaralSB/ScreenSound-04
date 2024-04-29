599482de6d6f1ff5f9c3f344242608f617c8c1c0
Maria Eduarda Amaral <mariaeduardaamaralsb@gmail.com>
2024-04-29 17:58:08 -0300

    Criado classe musica

---
+++ Modelos/Musicas.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace ScreenSound_04.Modelos
+{
+    internal class Musicas
+    {
+        [JsonPropertyName("song")]
+        public string? Nome { get; set; }
+        [JsonPropertyName("artist")]
+        public string? Artista { get; set; }
+        [JsonPropertyName("duration_ms")]
+        public int? Duracao{ get; set; }
+        [JsonPropertyName("genre")]
+        public string Genero { get; set; }
+
+        public void ExibirDetalhesdaMusica()
+        {
+            Console.WriteLine($"Artista: {Artista}");
+            Console.WriteLine($"Musica: {Nome}");
+            Console.WriteLine($"Duraçãp: {Duracao /1000}");
+            Console.WriteLine($"Genero musica: {Genero}");
+        }
+    }
+}
